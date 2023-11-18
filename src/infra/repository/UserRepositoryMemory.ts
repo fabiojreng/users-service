8812216cbde5b0c9f6b4f0b1c0153430d2ba11ab
fabiojreng <fabiojunioreloi@hotmail.com>
2023-11-18 20:14:19 -0300

    feat: Implements Repositories

--- src/infra/repository/UserRepositoryMemory.ts
+++ src/infra/repository/UserRepositoryMemory.ts
@@ -4,19 +4,22 @@ import User from "../../domain/entities/User";
 export default class UserRepositoryDBMemory implements ICreateUserRepository {
   users: User[] = [];
 
-  async findAll(): Promise<void | User[]> {
-    console.log(this.users);
-    return this.users;
-  }
   async save(user: User): Promise<void> {
     this.users.push(user);
   }
+
   async findByEmail(email: string): Promise<void | User> {
     const user = this.users.find((user) => user.email.getEmail() === email);
     return user;
   }
+
   async findById(id: string): Promise<void | User> {
     const user = this.users.find((user) => user.id === id);
     return user;
+  }
+
+  async findAll(): Promise<void | User[]> {
+    console.log(this.users);
+    return this.users;
   }
 }
--- src/infra/repository/UserRepositoryMogoDB.ts
+++ src/infra/repository/UserRepositoryMogoDB.ts
@@ -1,20 +1,50 @@
+import { ObjectId } from "mongodb";
 import IUserRepository from "../../aplication/repository/UserRepository";
 import User from "../../domain/entities/User";
 import AdapterMongoDB from "../dataBase/AdapterMongoDB";
 
 export default class UserRepositoryMongoDB implements IUserRepository {
-  constructor(private connection: AdapterMongoDB) {}
+  constructor(private mongo: AdapterMongoDB) {}
 
-  save(user: User): Promise<void> {
-    throw new Error("Method not implemented.");
+  async save(user: User): Promise<void> {
+    await this.mongo.connect();
+    const query = await this.mongo.query();
+    await query.collection("users").insertOne(user);
+    await this.mongo.close();
   }
-  findByEmail(email: string): Promise<void | User> {
-    throw new Error("Method not implemented.");
+
+  async findByEmail(email: string): Promise<void | User> {
+    await this.mongo.connect();
+    const query = await this.mongo.query();
+    const user = await query
+      .collection<User>("users")
+      .findOne({ typeUserEmail: email });
+
+    return user;
   }
-  findById(id: string): Promise<void | User> {
-    throw new Error("Method not implemented.");
+
+  async findById(id: string): Promise<void | User> {
+    await this.mongo.connect();
+    const query = await this.mongo.query();
+    console.log(id);
+
+    const user = await query
+      .collection<User>("users")
+      .findOne({ _id: new ObjectId(id) });
+
+    console.log(user);
+
+    return user;
   }
-  findAll(): Promise<void | User[]> {
-    throw new Error("Method not implemented.");
+
+  async findAll(): Promise<void | User[]> {
+    await this.mongo.connect();
+    const query = await this.mongo.query();
+    const users = await query.collection<User>("users").find({}).toArray();
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
+    await this.mongo.close();
   }
 }
