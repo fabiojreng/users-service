9cd699308d6b7428fe099d851f4e66af00a5709b
fabiojreng <fabiojunioreloi@hotmail.com>
2023-11-19 14:34:54 -0300

    feat: Update index

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,39 @@
 import express from "express";
-import CreateUserService from "./aplication/services/CreateUserService";
+import CreateUserUseCase from "./aplication/useCases/CreateUserUseCase";
+import GetAllUsersUseCase from "./aplication/useCases/GetAllUserUseCase";
 import CreateUserController from "./controllers/CreateUserController";
-import GetUsersService from "./aplication/services/GetUserService";
-import GetUserController from "./controllers/GetUserController";
 import GetAllUsersController from "./controllers/GetAllUsersController";
-import GetAllUsersService from "./aplication/services/GetAllUsersService";
-import UserRepositoryDBMemory from "./infra/repository/UserRepositoryMemory";
-
+import GetUserController from "./controllers/GetUserController";
+import AdapterMongoDB from "./infra/dataBase/AdapterMongoDB";
+import UserRepositoryMongoDB from "./infra/repository/UserRepositoryMogoDB";
+import GetUserUseCase from "./aplication/useCases/GetUserUseCase";
 const app = express();
 
 app.use(express.json());
 
-//const connection = new AdapterMongoDB();
-//const mongoDB = new UserRepositoryMongoDB(connection);
-
-const dbFake = new UserRepositoryDBMemory();
-
 app.get("/users", async (req, res) => {
-  const getAllUsersService = new GetAllUsersService(dbFake);
-  const getAllUsersController = new GetAllUsersController(getAllUsersService);
+  const connection = new AdapterMongoDB();
+  const mongoDB = new UserRepositoryMongoDB(connection);
+  const getAllUsersUseCase = new GetAllUsersUseCase(mongoDB);
+  const getAllUsersController = new GetAllUsersController(getAllUsersUseCase);
   const { statusCode, body } = await getAllUsersController.start();
   res.status(statusCode).send(body);
 });
 
 app.get("/user/:id", async (req, res) => {
-  const getUserService = new GetUsersService(dbFake);
-  const getUserController = new GetUserController(getUserService);
+  const connection = new AdapterMongoDB();
+  const mongoDB = new UserRepositoryMongoDB(connection);
+  const getUserUsecase = new GetUserUseCase(mongoDB);
+  const getUserController = new GetUserController(getUserUsecase);
   const { statusCode, body } = await getUserController.start(req);
   res.status(statusCode).send(body);
 });
 
 app.post("/user", async (req, res) => {
-  const createUserService = new CreateUserService(dbFake);
-  const createUserControler = new CreateUserController(createUserService);
+  const connection = new AdapterMongoDB();
+  const mongoDB = new UserRepositoryMongoDB(connection);
+  const createUserUseCase = new CreateUserUseCase(mongoDB);
+  const createUserControler = new CreateUserController(createUserUseCase);
   const { statusCode, body } = await createUserControler.start(req);
   res.status(statusCode).send(body);
 });
