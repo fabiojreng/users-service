b8772fcc9a5c3f5b4cdf2d73a49943a763134433
fabiojreng <fabiojunioreloi@hotmail.com>
2023-11-24 16:26:20 -0300

    feat: Implements interface the paramsUpdate

--- src/aplication/repository/UserRepository.ts
+++ src/aplication/repository/UserRepository.ts
@@ -1,9 +1,18 @@
 import User from "../../domain/entities/User";
 
+export interface paramsUpdate {
+  name?: string;
+  email?: string;
+  password?: string;
+  //registerCode: string;
+  //course: string;
+}
+
 export default interface IUserRepository {
   save(user: User): Promise<void>;
   findByEmail(email: string): Promise<void | User>;
   findById(id: string): Promise<void | User>;
   findAll(): Promise<void | User[]>;
   delete(id: string): Promise<void>;
+  update(id: string, params: paramsUpdate): Promise<void>;
 }
