b1a4988262c74b2a8cecca96cd5d10a3d0e62312
Fábio Júnior <fabiojunioreloi@hotmail.com>
2023-12-02 14:55:08 -0300

    feat: Remove DeleteUser and UpdateUser

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,8 @@ import MainController from "./infra/controller/MainController";
 import CreateUserUseCase from "./aplication/useCases/CreateUserUseCase";
 import GetAllUsersUseCase from "./aplication/useCases/GetAllUserUseCase";
 import GetUserUseCase from "./aplication/useCases/GetUserUseCase";
-import DeleteUserUseCase from "./aplication/useCases/DeleteUserUseCase";
 import AdapterMongoDB from "./infra/dataBase/AdapterMongoDB";
 import UserRepositoryMongoDB from "./infra/repository/UserRepositoryMogoDB";
-import UpdateUserUseCase from "./aplication/useCases/UpdateUserUseCase";
 
 const server = new ExpressAdapter();
 const connection = new AdapterMongoDB();
@@ -14,14 +12,10 @@ const mongoDB = new UserRepositoryMongoDB(connection);
 const createUser = new CreateUserUseCase(mongoDB);
 const getUser = new GetUserUseCase(mongoDB);
 const getAllUsers = new GetAllUsersUseCase(mongoDB);
-const deleteUser = new DeleteUserUseCase(mongoDB);
-const updateUser = new UpdateUserUseCase(mongoDB);
 new MainController(
   createUser,
   getUser,
   getAllUsers,
-  deleteUser,
-  updateUser,
   server
 );
 server.listen(3333);
