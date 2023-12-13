1b6c9cb0b6962888d6e842a278d0774b49ecaa64
Fábio Júnior <fabiojunioreloi@hotmail.com>
2023-12-13 11:30:41 -0300

    feat: Create swagger configuration

--- src/infra/http/ExpressAdapter.ts
+++ src/infra/http/ExpressAdapter.ts
@@ -1,11 +1,18 @@
 import express, { Request, Response } from "express";
 import HttpServer from "./HttpServer";
+import swaggerUI from "swagger-ui-express";
+import swaggerDocs from "/home/fabiojunior/Área de Trabalho/users-service/src/swagger.json";
 
 export default class ExpressAdapter implements HttpServer {
   app: any;
   constructor() {
     this.app = express();
     this.app.use(express.json());
+    this.app.use(
+      "/documentation",
+      swaggerUI.serve,
+      swaggerUI.setup(swaggerDocs)
+    );
   }
 
   // eslint-disable-next-line @typescript-eslint/ban-types
